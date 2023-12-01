ea2d89f22d945cd2bc268f792f679a7653d7b60e
Amin Zohrabi Far <aminzf@gmail.com>
2023-12-01 16:23:25 +0330

    Add files via upload

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"ginblog/config"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"net/http"
+)
+
+func main() {
+	configs := configSet()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "pong",
+			"app_name": configs.App.Name,
+		})
+	})
+	err := r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port)) // listen and serve on 0.0.0.0:8080
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func configSet() config.Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error reading the configs")
+	}
+	var configs config.Config
+	err := viper.Unmarshal(&configs)
+	if err != nil {
+		fmt.Printf("unable to decode into struct, %v", err)
+	}
+	return configs
+}
