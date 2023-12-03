562994e66b5c8da66868fc2d61bd2af2f8a3091f
aminzohrabifar <aminzf@gmail.com>
2023-12-03 06:22:49 +0330

    create internal modules home routes create pkg register routing

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -3,20 +3,11 @@ package bootstrap
 import (
 	"ginblog/pkg/config"
 	"ginblog/pkg/routing"
-	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
-	"net/http"
 )
 
 func Serve() {
 	config.Set()
 	routing.Init()
-	router := routing.GetRouter()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message":  "pong",
-			"app_name": viper.Get("App.Name"),
-		})
-	})
+	routing.RegisterRoutes()
 	routing.Serve()
 }
--- pkg/routing/routing.go
+++ pkg/routing/routing.go
@@ -1,10 +1,16 @@
 package routing
 
-import "github.com/gin-gonic/gin"
+import (
+	"ginblog/internal/modules/home/routes"
+	"github.com/gin-gonic/gin"
+)
 
 func Init() {
 	router = gin.Default()
 }
 func GetRouter() *gin.Engine {
 	return router
+}
+func RegisterRoutes() {
+	routes.Routes(GetRouter())
 }
