c34f78e892f6896eaa3b1c6a0ff8817657d7a4ac
aminzohrabifar <aminzf@gmail.com>
2023-12-05 06:47:22 +0330

    render static file

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -4,11 +4,13 @@ import (
 	"ginblog/pkg/config"
 	"ginblog/pkg/html"
 	"ginblog/pkg/routing"
+	"ginblog/pkg/static"
 )
 
 func Serve() {
 	config.Set()
 	routing.Init()
+	static.LoadStatic(routing.GetRouter())
 	html.LoadHTML(routing.GetRouter())
 	routing.RegisterRoutes()
 	routing.Serve()
