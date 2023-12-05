24d6e7787a5cc59a44e3bbca985374583093dcee
aminzohrabifar <aminzf@gmail.com>
2023-12-05 06:24:19 +0330

    create pageHTML structure create about and home page tmpl

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -2,12 +2,14 @@ package bootstrap
 
 import (
 	"ginblog/pkg/config"
+	"ginblog/pkg/html"
 	"ginblog/pkg/routing"
 )
 
 func Serve() {
 	config.Set()
 	routing.Init()
+	html.LoadHTML(routing.GetRouter())
 	routing.RegisterRoutes()
 	routing.Serve()
 }
