374740dc820cc4681417d4ff96e827c841aa680d
aminzohrabifar <aminzf@gmail.com>
2023-12-07 14:18:40 +0330

    Create homeController

---
+++ internal/modules/home/controllers/home_controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"ginblog/pkg/html"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Controller struct {
+}
+
+func New() *Controller {
+	return &Controller{}
+}
+func (controller *Controller) Index(c *gin.Context) {
+	html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
+		"title": "Home Page",
+	})
+}
--- internal/modules/home/routes/routes.go
+++ internal/modules/home/routes/routes.go
@@ -1,17 +1,15 @@
 package routes
 
 import (
+	"ginblog/internal/modules/home/controllers"
 	"ginblog/pkg/html"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func Routes(router *gin.Engine) {
-	router.GET("/", func(c *gin.Context) {
-		html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
-			"title": "Home Page",
-		})
-	})
+	homeController := controllers.New()
+	router.GET("/", homeController.Index)
 
 	router.GET("/about", func(c *gin.Context) {
 		html.Render(c, http.StatusOK, "modules/home/html/about", gin.H{
